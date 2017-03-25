c1604bd0be69c8f16d3ee8554540f90d8f646096
feldtsen <feldtsen@gmail.com>
2017-03-25 18:13:48 +0100

    media

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 window.addEventListener('load', ()=>{
-    
+   var background = document.getElementById('background');
+
+// Fix background image jump on mobile
+if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+  background.style.top = 'auto';
+  background.style.bottom = 0;
+
+  window.onresize = sizeBackground;
+  sizeBackground();
+}
+
+function sizeBackground() {
+  background.style.height = screen.height;
+} 
 });
