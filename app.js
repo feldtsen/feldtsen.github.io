3cce96200b7450589bc0c1cc8aae669a11fe1141
feldtsen <feldtsen@gmail.com>
2017-03-26 18:09:30 +0200

    pages

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-window.addEventListener('load', e=>{
-   let menuButton = document.getElementsByClassName('menuButton');
-    for(let i = 0; i < menuButton.length; i++){
+window.addEventListener('load', e => {
+    let menuButton = document.getElementsByClassName('menuButton')
+        , menuOptions = document.getElementsByClassName('menuOption');
+    for (let i = 0; i < menuButton.length; i++) {
         menuButton[i].addEventListener('click', e => {
             document.getElementsByClassName('menuPage')[0].classList.toggle('active');
-        })
+        });
+    }
+    for (let i = 0; i < menuOptions.length; i++) {
+        menuOptions[i].addEventListener('click', e => {
+            menuOptions[i].classList.toggle('activePage');
+        });
     }
 });
