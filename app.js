0e353b63a4de4bb70e6264bb749285e5e4a2856b
feldtsen <feldtsen@gmail.com>
2017-03-26 03:46:45 +0200

    menu page

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-window.addEventListener('load', ()=>{
-    
+window.addEventListener('load', e=>{
+   let menuButton = document.getElementsByClassName('menuButton');
+    for(let i = 0; i < menuButton.length; i++){
+        menuButton[i].addEventListener('click', e => {
+            document.getElementsByClassName('menuPage')[0].classList.toggle('active');
+        })
+    }
 });
