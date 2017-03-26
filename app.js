0f0213174d5e74f76b933eb725539ab02b3dcbcd
feldtsen <feldtsen@gmail.com>
2017-03-27 00:36:38 +0200

    menu button icon

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 window.addEventListener('load', e => {
-    let menuButton = document.getElementsByClassName('menuButton')
-        , menuOptions = document.getElementsByClassName('menuOption');
-    for (let i = 0; i < menuButton.length; i++) {
-        menuButton[i].addEventListener('click', e => {
+    let menuButton = document.getElementById('menuButton')
+        , menuOptions = document.getElementsByClassName('menuOption')
+        , pagestatus = false
+        , currentPage, menuStatus = false;
+    menuButton.addEventListener('click', e => {
+        menuToggle(!menuStatus);
+        if (pagestatus) {
+            menuOptions[currentPage].classList.toggle('activePage');
+            pagestatus = false;
+        }
+        else {
             document.getElementsByClassName('pages')[0].classList.toggle('active');
-        });
-    }
+        }
+    });
     for (let i = 0; i < menuOptions.length; i++) {
         menuOptions[i].addEventListener('click', e => {
-            console.log(i);
-            menuOptions[i].classList.toggle('activePage');
+            menuOptions[i].classList.add('activePage');
+            currentPage = i;
+            if (!pagestatus) menuToggle(!menuStatus);
+            pagestatus = true;
         });
     }
+
+    function menuToggle(x) {
+        if (x) {
+            menuButton.innerHTML = `<i class="fa fa-times" aria-hidden="true"></i>`;
+        }
+        else {
+            menuButton.innerHTML = `<i class="fa fa-bars" aria-hidden="true"></i>`;
+        }
+        menuStatus = !menuStatus;
+    }
 });
+/*
+<i class="fa fa-bars" aria-hidden="true"></i>
+*/
