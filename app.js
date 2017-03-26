4969dca561ceb5fc0e6c6966b5a3ce42a8a65bdb
feldtsen <feldtsen@gmail.com>
2017-03-26 20:34:12 +0200

    pages

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ window.addEventListener('load', e => {
         , menuOptions = document.getElementsByClassName('menuOption');
     for (let i = 0; i < menuButton.length; i++) {
         menuButton[i].addEventListener('click', e => {
-            document.getElementsByClassName('menuPage')[0].classList.toggle('active');
+            document.getElementsByClassName('pages')[0].classList.toggle('active');
         });
     }
     for (let i = 0; i < menuOptions.length; i++) {
         menuOptions[i].addEventListener('click', e => {
+            console.log(i);
             menuOptions[i].classList.toggle('activePage');
         });
     }
