24b27e9f191eff0bd722efcbb9229cb0e0975cea
feldtsen <feldtsen@gmail.com>
2017-05-30 14:47:11 +0200

    newversion

--- feldtsen/src/App.js
+++ feldtsen/src/App.js
@@ -12,6 +12,8 @@ export default class App extends Component {
             animationPoints: 0,
             letterArray: ['f', 'e', 'l' , 'd' , 't', 's' ,'e', 'n'],
             numberArray: [.7, .3, 1, .8, .7, 1, .2, .8],
+            currentPage: 1,
+            touchMove: 0,
             pageStyle: {
                 width: window.innerWidth,
                 height: window.innerHeight,
@@ -27,13 +29,26 @@ export default class App extends Component {
         window.addEventListener(wheelevt, this.handleScroll, {passive: true});
         window.addEventListener("keydown", this.handleKeydown, {passive:true});
         window.addEventListener("resize", this.responsiveChecker);
+        window.addEventListener("touchmove", this.handleMove, {passive:true});
     }
 
     componentWillUnmount(){
         window.removeEventListener(wheelevt, this.handleScroll);
         window.removeEventListener("keydown", this.handleKeydown);
         window.removeEventListener("resize", this.responsiveChecker);
+        window.removeEventListener("touchmove", this.handleMove);
     }
+
+    handleMove = (e) => {
+        let current = this.state;
+        let yLocation = e.targetTouches[0].screenY;
+        console.log(yLocation);
+        yLocation > current.touchMove ? current.location ++ : current.location --;
+        current.touchMove = yLocation;
+        if(current.location < 0) current.location = 0;
+        else if (current.location > this.state.maxLocation) current.location = this.state.maxLocation;
+        this.pageTracker(current.location)
+    };
 
     handleScroll=(e)=>{
         const direction = (e.detail<0 || e.wheelDelta>0) ? 1 : -1,
@@ -73,6 +88,7 @@ export default class App extends Component {
         switch (currentLocation) {
             case 49:
                 if(current.pageStyle.classNameMain !== 'intro') {
+                    current.currentPage = 1;
                     current.pageStyle.classNameMain = 'intro';
                     current.pageStyle.classNameContent = 'introContent';
                     current.pageStyle.classNameBar = 'introBar';
@@ -82,6 +98,7 @@ export default class App extends Component {
                 break;
             case 50:
                 if(current.pageStyle.classNameMain !== 'projects'){
+                    current.currentPage = 2;
                     current.pageStyle.classNameMain = 'projects';
                     current.pageStyle.classNameContent = 'projectsContent';
                     current.pageStyle.classNameBar = 'projectsBar';
@@ -92,6 +109,7 @@ export default class App extends Component {
                 break;
             case 99:
                 if(current.pageStyle.classNameMain !== 'projects'){
+                    current.currentPage = 2;
                     current.pageStyle.classNameMain = 'projects';
                     current.pageStyle.classNameContent = 'projectsContent';
                     current.pageStyle.classNameBar = 'projectsBar';
@@ -102,6 +120,7 @@ export default class App extends Component {
                 break;
             case 100:
                 if(current.pageStyle.classNameMain !== 'contact') {
+                    current.currentPage = 3;
                     current.pageStyle.classNameMain = 'contact';
                     current.pageStyle.classNameContent = 'contactContent';
                     current.pageStyle.classNameBar = 'contactBar';
@@ -149,20 +168,27 @@ export default class App extends Component {
                   }}>
 
                   <div className={this.state.pageStyle.classNameContent} style={{width: `${this.state.pageStyle.width * 0.95}px`,
-                      height: `${this.state.pageStyle.height * 0.8}px`,
+                      height: `${this.state.pageStyle.height}px`,
                   }}>
                       <h1>
                           {
                               this.state.letterArray.map((letter, i)=>{return <span key={letter + i} style={{
-                                  transform: `translateY(${(this.state.animationPoints * this.state.numberArray[i])/3}px)`
+                                  transform: `translateY(${(this.state.animationPoints * this.state.numberArray[i])/2}px)`
                               }}>{letter}</span>})
                           }
                       </h1>
                   </div>
-                  <div className="progress" style={{width: `${this.state.pageStyle.width * 0.05}px`,
-                      height: `${this.state.pageStyle.height * 0.8}px`,
+                  <div className="progress" style={{
+                      width: `${this.state.pageStyle.width * 0.05}px`,
+                      height: `${this.state.pageStyle.height}px`,
                   }}>
-                        <div className={this.state.pageStyle.classNameBar} style={{height: `${this.state.pageStyle.progress}%`}}> </div>
+                        <div className={this.state.pageStyle.classNameBar} style={{height: `${this.state.pageStyle.progress}%`}}>
+                            <div>
+                                <h3>{this.state.currentPage}</h3>
+                                <h3>/</h3>
+                                <h3>3</h3>
+                            </div>
+                        </div>
                   </div>
 
               </main>
