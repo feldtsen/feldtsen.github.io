c23e7340a9d00cfe559fb41dc8ae3b69cebb0d65
feldtsen <feldtsen@gmail.com>
2019-07-16 11:48:07 +0200

    relative intro

--- src/Intro.js
+++ src/Intro.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import Grid from '@material-ui/core/Grid'
 
 function Intro(props) {
     let user = props.user[0];
     return (
-        <div style={{height: props.windowHeight * 0.6}} className="Intro">
-            <h1>{user.name}</h1>
-            <h3>{user.subTitle}</h3>
-        </div>
+        <Grid container direction='column' style={{
+            height: props.windowHeight * 0.6,
+            fontSize: props.windowWidth >= 1000 ? props.windowWidth * 0.04 : 1.5 + 'em',
+                paddingTop: props.windowHeight * 0.05,
+                paddingLeft: props.windowWidth * 0.05
+        }} className="Intro">
+            <Grid item>{user.name}</Grid>
+            <Grid item>{user.subTitle}</Grid>
+        </Grid>
     );
 }
 
