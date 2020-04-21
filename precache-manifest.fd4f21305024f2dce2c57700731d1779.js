61665aad137d60f259bfd9ecfcb6477548b8062c
feldtsen <feldtsen@gmail.com>
2020-04-21 16:19:57 +0200

    Updates

---
+++ precache-manifest.fd4f21305024f2dce2c57700731d1779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3924960578e83aed3ded2bea1e253785",
+    "url": "/feldtsen/feldtsen.github.io.git/index.html"
+  },
+  {
+    "revision": "22ae0cbc6de85715faef",
+    "url": "/feldtsen/feldtsen.github.io.git/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "5d70f0afb126e8d5ccc8",
+    "url": "/feldtsen/feldtsen.github.io.git/static/js/2.4f901e17.chunk.js"
+  },
+  {
+    "revision": "4f4f8e381b1360c68c2230a854023450",
+    "url": "/feldtsen/feldtsen.github.io.git/static/js/2.4f901e17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ae0cbc6de85715faef",
+    "url": "/feldtsen/feldtsen.github.io.git/static/js/main.be6fb9b4.chunk.js"
+  },
+  {
+    "revision": "1404c8d12d30ccbfd2f9",
+    "url": "/feldtsen/feldtsen.github.io.git/static/js/runtime-main.ef3b63a7.js"
+  }
+]);
